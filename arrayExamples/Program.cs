187f8c6eb79caf734f424c8a0ed06993a443d323
krisiivanova24 <krisiivanova24@abv.bg>
2023-02-04 14:35:35 +0200

    first basic examples of array

---
+++ arrayExamples/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace arrayExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //първи начин за четене и печатане на масив
+            /*int[] intArray = new int[5];
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                intArray[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                Console.Write(intArray[i] + ", ");
+            }*/
+
+            // !! втори начин за четене и печатане на масив !!!
+            int[] intArray2 = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            Console.WriteLine(string.Join(", ", intArray2));
+
+            double[] doubleArray = Console.ReadLine().Split(',').Select(double.Parse).ToArray();
+
+            string[] stringArray = Console.ReadLine().Split().ToArray();
+            Console.WriteLine(string.Join(" - ", stringArray));
+        }
+    }
+}
